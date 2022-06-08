8fc2822e6866841a079cc7c2e6c27c0a2f8705b4
Erik Riggs <riggs.erik@gmail.com>
2022-06-08 10:16:37 -0600

    Demonstrates index positions

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,5 @@
 countries_visited = ["Vietnam", "Honduras", "Denmark", "Scotland"]
+#index positions Vietnam 0, Honduras 1, Denmark 2, Scotland 3
 ages = [2, 3, 5, 8]
 payments = [24.12, 34.55, 64.24, 78.93]
 online = [true, true, false]
